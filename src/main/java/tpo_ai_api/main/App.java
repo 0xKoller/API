5f18f3ba2d9448aa752cffb27d4476ae822222ee
juquinteros <39176604+juquinteros@users.noreply.github.com>
2023-09-18 15:26:53 -0300

    Update App.java
    Se agrego en comentarios la llamada a los daos aun me da lo siguiente "Exception in thread "main" org.hibernate.MappingException: Unknown entity: tpo_ai_api.model.Usuario" o "Exception in thread "main" org.hibernate.MappingException: Unknown entity: tpo_ai_api.model.Edificio"

--- src/main/java/tpo_ai_api/main/App.java
+++ src/main/java/tpo_ai_api/main/App.java
@@ -6,9 +6,22 @@ import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import tpo_ai_api.model.Direccion;
 import tpo_ai_api.model.Edificio;
-
+/*import tpo_ai_api.model.Usuario;*/
 public class App {
     public static void main(String[] args) {
+
+/*por DAO*/
+        /*DAOUsuarioImpl dao = new DAOUsuarioImpl();
+    	Usuario usuario = new Usuario("juquin","1234",3333333,5555555,"juanca@juanca.com",true,null,null);
+    	dao.save(usuario);
+    	System.out.println(usuario);
+    	
+    	
+    	DAOEdificioImpl dao2 = new DAOEdificioImpl();
+    	Edificio edificio2 = new Edificio("salta",new Direccion("CABA","monserrat","salta",221));
+    	dao2.save(edificio2);
+    	System.out.println(edificio2);*/
+        
         Configuration conf = new Configuration().configure();
         conf.addAnnotatedClass(Edificio.class);
         SessionFactory sf = conf.buildSessionFactory();

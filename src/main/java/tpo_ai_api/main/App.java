3b6e55751c17ba072649837ad810bde6c30d6a50
juquinteros <39176604+juquinteros@users.noreply.github.com>
2023-09-19 16:49:49 -0300

    cambios en unidades

--- src/main/java/tpo_ai_api/main/App.java
+++ src/main/java/tpo_ai_api/main/App.java
@@ -7,6 +7,8 @@ import org.hibernate.cfg.Configuration;
 import tpo_ai_api.dao.*;
 import tpo_ai_api.model.Direccion;
 import tpo_ai_api.model.Edificio;
+import tpo_ai_api.model.EstadoUnidad;
+import tpo_ai_api.model.Unidad;
 import tpo_ai_api.model.Usuario;
 
 public class App {
@@ -76,7 +78,38 @@ try {
 			System.out.println(dao31 );
 		}
      
-    	
+    	EstadoUnidad est1= EstadoUnidad.ALQUILADA;
+    	EstadoUnidad est2= EstadoUnidad.HABITADA;
+       DAOUnidad daou2 = new DAOUnidadImpl();
+  	   Unidad unidad2 = new Unidad(6,1,est1,1,2);
+  	   Unidad unidad3 = new Unidad(12,4,est2,2,1);
+  	   try {
+  		daou2.save(unidad2,session);
+  	} catch (Exception e) {
+  		// TODO Auto-generated catch block
+  		e.printStackTrace();
+  	}
+  	   System.out.println(unidad2);
+  	   try {
+  		daou2.save(unidad3,session);
+  	} catch (Exception e) {
+  		// TODO Auto-generated catch block
+  		e.printStackTrace();
+  	}
+  	   System.out.println(unidad3);  	    	
+  	    			
+  	   DAOUnidad daou3 = new DAOUnidadImpl();
+  	   List<Unidad> unidades2 = null;
+  	try {
+  		unidades2 = daou3.getUnidades(session);
+  	} catch (Exception e) {
+  		// TODO Auto-generated catch block
+  		e.printStackTrace();
+  	}
+  	    	for (Unidad daou31 : unidades2) {
+  				System.out.println(daou31 );
+  			}
+  			
         /*Configuration conf = new Configuration().configure();
         conf.addAnnotatedClass(Edificio.class);
         SessionFactory sf = conf.buildSessionFactory();
